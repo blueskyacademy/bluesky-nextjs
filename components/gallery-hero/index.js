c5292b8978d0492e579fff89cd61c9110fb88fc3
Minhchau0108 <74655110+Minhchau0108@users.noreply.github.com>
2022-01-01 21:43:24 +0700

    add plugin facebook (#15)

--- components/gallery-hero/index.js
+++ components/gallery-hero/index.js
@@ -1,77 +1,86 @@
 import Link from "next/link";
 
-const GalleryHero = ({ title, description, hasButton = false, url }) => {
+const GalleryHero = ({
+  id,
+  className,
+  title,
+  description,
+  hasButton = false,
+  url,
+}) => {
   return (
-    <section className="px-4 pt-12 sm:px-6 lg:px-8">
-      <div className="max-w-screen-xl mx-auto">
-        <div className="relative">
-          <h2 className="max-w-3xl mx-auto text-center text-purple-900 text-5xl font-bold">
-            {title}
-          </h2>
-          <p className="max-w-3xl mx-auto mt-4 text-xl leading-relaxed text-center text-purple-800 sm:mt-5">
-            {description}
-          </p>
-        </div>
-
-        <div className="grid grid-flow-row-dense grid-cols-2 gap-2 sm:grid-cols-3 mt- sm:mt-12 lg:mt-12 sm:gap-3 md:gap-4 lg:gap-6">
-          <img
-            className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
-            alt="School grid 01"
-            src="images/primary.png"
-          />
-
-          <img
-            className="object-cover w-full col-span-2 rounded-2xl lg:h-80 h-30vw"
-            src="images/kindergarten.png"
-            alt="School grid 02"
-          />
-
-          <img
-            className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
-            src="images/secondary.png"
-            alt="School grid 03"
-          />
-
-          <img
-            className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
-            src="images/gallery-hero-01.png"
-            alt="School grid 04"
-          />
-
-          <img
-            className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
-            src="images/gallery-hero-02.png"
-            alt="School grid 05"
-          />
-        </div>
-
-        {hasButton && (
-          <div className="flex justify-center mt-12 sm:mt-16">
-            <Link href={url}>
-              <button className="text-lg font-medium text-purple-900 bg-purple-200 sm:mt-0 sm:ml-6 leading-normal inline-flex items-center justify-center px-8 py-3 duration-300 ease-in-out rounded-full outline-none hover:text-white hover:bg-purple-600 group">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mr-3 text-purple-600 duration-300 ease-in-out group-hover:text-purple-50"
-                  width="44"
-                  height="44"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <line x1="12" y1="5" x2="12" y2="19" />
-                  <line x1="5" y1="12" x2="19" y2="12" />
-                </svg>
-                View more
-              </button>
-            </Link>
+    <div id={id} className={className}>
+      <section className="px-4 pt-12 sm:px-6 lg:px-8">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="relative">
+            <h2 className="max-w-3xl mx-auto text-center text-purple-900 text-5xl font-bold">
+              {title}
+            </h2>
+            <p className="max-w-3xl mx-auto mt-4 text-xl leading-relaxed text-center text-purple-800 sm:mt-5">
+              {description}
+            </p>
           </div>
-        )}
-      </div>
-    </section>
+
+          <div className="grid grid-flow-row-dense grid-cols-2 gap-2 sm:grid-cols-3 mt- sm:mt-12 lg:mt-12 sm:gap-3 md:gap-4 lg:gap-6">
+            <img
+              className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
+              alt="School grid 01"
+              src="images/primary.png"
+            />
+
+            <img
+              className="object-cover w-full col-span-2 rounded-2xl lg:h-80 h-30vw"
+              src="images/kindergarten.png"
+              alt="School grid 02"
+            />
+
+            <img
+              className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
+              src="images/secondary.png"
+              alt="School grid 03"
+            />
+
+            <img
+              className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
+              src="images/gallery-hero-01.png"
+              alt="School grid 04"
+            />
+
+            <img
+              className="object-cover w-full rounded-2xl lg:h-80 h-30vw"
+              src="images/gallery-hero-02.png"
+              alt="School grid 05"
+            />
+          </div>
+
+          {hasButton && (
+            <div className="flex justify-center mt-12 sm:mt-16">
+              <Link href={url}>
+                <button className="text-lg font-medium text-purple-900 bg-purple-200 sm:mt-0 sm:ml-6 leading-normal inline-flex items-center justify-center px-8 py-3 duration-300 ease-in-out rounded-full outline-none hover:text-white hover:bg-purple-600 group">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 mr-3 text-purple-600 duration-300 ease-in-out group-hover:text-purple-50"
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <line x1="12" y1="5" x2="12" y2="19" />
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                  </svg>
+                  View more
+                </button>
+              </Link>
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
   );
 };
 
--- components/navbar/dropdown-link.js
+++ components/navbar/dropdown-link.js
@@ -1,8 +1,38 @@
+import Link from "next/link";
 import { useState } from "react";
+import { NavLink } from "./nav-link";
 
-const DropdownLink = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  return <div></div>;
+const DropdownLink = ({ title, slug, path, subMenu = [] }) => {
+  return (
+    <div className="dropdown group">
+      <NavLink href={path} title={title} exact></NavLink>
+      {subMenu.length > 0 ? (
+        <div className="pt-2">
+          <div
+            className="absolute z-20 max-w-xs p-2 -translate-x-1/3 bg-white border shadow-lg left-1/2 border-gray-100 rounded-2xl dropdown-content"
+            style={{ minWidth: "220px" }}
+          >
+            {subMenu.map((item, idx) => (
+              <div key={`submenu-${idx}`}>
+                <Link href={item.href} key={`${item.title}-${idx}`}>
+                  <a className="block w-full py-3 transition duration-200 ease-in-out rounded-xl sm:px-5 hover:bg-purple-25 group">
+                    <h5 className="text-md font-semibold text-purple-600 whitespace-nowrap">
+                      {item.title}
+                    </h5>
+                  </a>
+                </Link>
+                {idx < subMenu.length - 1 && (
+                  <hr className="my-1 border-purple-200/30 sm:my-2" />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="pt-2"></div>
+      )}
+    </div>
+  );
 };
 
 export default DropdownLink;
