9b97c959424c7a9059758985673b24788b87189e
Minhchau0108 <minhchau3315@gmail.com>
2022-01-04 11:08:59 +0700

    fix plugin facebook customer

--- components/facebook/index.js
+++ components/facebook/index.js
@@ -1,18 +1,31 @@
-import { useEffect } from "react";
-import { cleanup, init } from "./fb";
-
+/* eslint-disable @next/next/inline-script-id */
+import Script from "next/script";
 const Facebook = () => {
-  useEffect(() => {
-    console.log("Facebook1");
-    init();
-
-    return () => {
-      cleanup();
-    };
-  }, []);
   return (
     <div>
       <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <Script strategy="lazyOnload">
+        {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "113338784471007");
+            chatbox.setAttribute("attribution", "biz_inbox");
+      
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+      </Script>
     </div>
   );
 };
